22b15d554bc4d5d0ef68675df99adb0954e6de67
Eugene Svalukhin <eugene.svalukhin@gmail.com>
2019-06-13 15:05:45 +0600

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,8 +34,8 @@ public class AppTest
     public void testAppMain()
     {
         App.main(null);
-        Thread.sleep(1000L);
         try {
+            Thread.sleep(1000L);1
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
