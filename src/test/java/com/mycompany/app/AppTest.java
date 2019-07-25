4371ec6fc1d6f88486de45e34ae10bbb4ce30639
Eugene Svalukhin <eugene.svalukhin@gmail.com>
2019-07-25 17:40:54 +0600

    Update AppTest.java
    Return back test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello orld!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (Exception e) {
-            fail("\"message\" is not \"ello World!\"");
+            fail("\"message\" is not \"Hello World!\"");
         }
     }
 
